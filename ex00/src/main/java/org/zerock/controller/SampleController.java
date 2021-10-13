3e61d4d3246a9fab7ea6222aec5cd33b710165bd
walle <dlauddn95@gmail.com>
2021-10-13 16:00:42 +0900

    :>

--- ex00/src/main/java/org/zerock/controller/SampleController.java
+++ ex00/src/main/java/org/zerock/controller/SampleController.java
@@ -3,18 +3,23 @@ package org.zerock.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
-
 @Controller
 @RequestMapping("/sample")
 @Log4j
@@ -104,5 +109,66 @@ public class SampleController {
 		
 		return "/sample/ex04";
 	}
+	
+	//호출하는 URL과 동일한 이름의 JSP를 page를 반환함
+	@GetMapping("/ex05")
+	public void ex05() {
+		
+		log.info("/ex05.........");
+	}
+	
+	//스프링 MVC는 자동으로 브라우저에 JSON 타입으로 객체를 변환해서 전달하게 됩니다.
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		
+		log.info("/ex06..........");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
+	
+	//HTTP 프로토콜의 헤더를 다루는 경우 ResponseEntity를 통해서 원하는 헤더 정보나 데이터를 전달 할 수 있음
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		
+		log.info("/ex07.......");
+		
+		//{"name": 홍길동}
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		
+		log.info("/exUpload........");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		
+		files.forEach(file ->{
+			log.info("------------------------");
+			log.info("name: " + file.getOriginalFilename());
+			log.info("size: " + file.getSize());
+		});
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
