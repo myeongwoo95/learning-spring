68f5bfb3839b9c4f890851f06d1e06ca9015390f
walle <dlauddn95@gmail.com>
2021-11-14 12:50:37 +0900

    security

--- ex04/src/main/java/org/zerock/security/CustomNoOpPasswordEncoder.java
+++ ex04/src/main/java/org/zerock/security/CustomNoOpPasswordEncoder.java
@@ -1,9 +1,12 @@
 package org.zerock.security;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import lombok.extern.log4j.Log4j;
 
+//사용안함, 실제로 인코딩은 안하는 클래스임 테스트용
 @Log4j
-public class CustomNoOpPasswordEncoder {
+public class CustomNoOpPasswordEncoder implements PasswordEncoder{
 	
 	public String encode(CharSequence rawPassword) {
 		
---
+++ ex04/src/main/java/org/zerock/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package org.zerock.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zerock.mapper.MemberMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+//security-context.xml 에 등록해주어야함
+public class CustomUserDetailsService implements UserDetailsService{ 
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		log.warn("Load User By UserName : " + username);
+		
+		return null;
+	}
+	
+}
