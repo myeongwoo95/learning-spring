1ae8a165eb9e76fb6c3e4a941feb4383d66166e7
myeongwoo Lee <48290545+myeongwoo95@users.noreply.github.com>
2022-05-27 09:52:09 +0900

    123

--- ex00/src/main/java/org/zerock/controller/HomeController.java
+++ ex00/src/main/java/org/zerock/controller/HomeController.java
@@ -21,6 +21,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * test 입니다
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
